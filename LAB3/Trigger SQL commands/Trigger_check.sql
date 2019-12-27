e06641d923f7a9ea7efa24ea5b34a6666db5970b
Ilya Belyaev <55363113+BelyaevIlyaUkraine@users.noreply.github.com>
2019-12-27 11:09:42 +0200

    Add files via upload

---
+++ LAB3/Trigger SQL commands/Trigger_check.sql
@@ -0,0 +1,4 @@
+--Delete from "Network" where "Name" like 'Sp%'
+--INSERT INTO "Network" ("Name","Owner") 
+	--VALUES ('Spirit3','Belyaev Ilya Dmitrievich');
+Update "Network" Set "Owner" = 'Privet';
