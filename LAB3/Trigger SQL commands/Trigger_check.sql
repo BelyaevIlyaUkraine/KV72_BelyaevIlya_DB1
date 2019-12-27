735e6a8e43b25ea08abb4e474411f6e78cdeaea8
Ilya Belyaev <55363113+BelyaevIlyaUkraine@users.noreply.github.com>
2019-12-27 13:32:04 +0200

    Delete Trigger_check.sql

--- LAB3/Trigger SQL commands/Trigger_check.sql
+++
@@ -1,4 +0,0 @@
---Delete from "Network" where "Name" like 'Sp%'
---INSERT INTO "Network" ("Name","Owner") 
-	--VALUES ('Spirit3','Belyaev Ilya Dmitrievich');
-Update "Network" Set "Owner" = 'Privet';
